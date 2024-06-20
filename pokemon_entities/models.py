0a7da18a7d963f744ca0106c46f34bcbb6682d44
Aleksandr Skripko <skripkoae@gmail.com>
2024-06-20 09:47:37 +0300

    rename Pokemon FK 'start_form' to 'previous_evolution'

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,7 +7,7 @@ class Pokemon(models.Model):
     description = models.TextField(default='')
     title_en = models.CharField(max_length=200, default='')
     title_jp = models.CharField(max_length=200, default='')
-    start_form = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    previous_evolution = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.title_ru}'
